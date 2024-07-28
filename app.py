64c129ebaa19b085d3ea80034858e2e6e246f06c
Christian Lopez <cflopez.86@gmail.com>
2024-07-28 16:53:26 -0300

    Simplificar app.py

--- app.py
+++ app.py
@@ -1,23 +1,12 @@
-import os
-from flask import Flask, jsonify
-import psycopg2
-from psycopg2.extras import RealDictCursor
+from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-
-# Conexión a la base de datos
-DATABASE_URL = os.environ.get('DATABASE_URL')
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route('/')
-def index():
+def hello_world():
     return 'Hello, world!'
 
-@app.route('/test')
-def test():
-    return jsonify({"message": "API is working"}), 200
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
 
