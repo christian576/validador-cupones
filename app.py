fc7977261fc2cc75deca0fee9b346572b12a2ec3
Christian Lopez <cflopez.86@gmail.com>
2024-07-28 17:03:20 -0300

    Changed from Gunicorn to Waitress

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = b'\xd9\x00\x02I\x19\x1a\xe6(\xb8\xa5\x0c\x9c\xec\xa8\x9c)\x04\xca\xb8\xee\x10\x10\xc1\xff'
 
 @app.route('/')
 def home():
-    return 'Hola, Mundo!'
+    return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8000)
