92f4e34b9556e43df5d509cf1df59b467bdfdd7f
Christian Lopez <cflopez.86@gmail.com>
2024-07-28 16:59:18 -0300

    Actualizando Procfile y app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = b'\xd9\x00\x02I\x19\x1a\xe6(\xb8\xa5\x0c\x9c\xec\xa8\x9c)\x04\xca\xb8\xee\x10\x10\xc1\xff'
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def home():
+    return 'Hola, Mundo!'
 
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(debug=True)
