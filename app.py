90f5624cae1e748fe4230b21e18ba877b3469c30
Christian Lopez <cflopez.86@gmail.com>
2024-07-28 17:19:59 -0300

    Configure Flask to run in production without Gunicorn

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return jsonify({"message": "This is a test endpoint"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
