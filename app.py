6a33753ea02ad5a78e281152c377acdc628281dd
Christian Lopez <cflopez.86@gmail.com>
2024-07-28 17:13:47 -0300

    Change to use Flask development server

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
