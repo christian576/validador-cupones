479fab54ca76e4b4db52416da58282f34f4e838e
Christian Lopez <cflopez.86@gmail.com>
2024-07-29 06:41:55 -0300

    Configuración de waitress desde el comando de inicio

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-import os
-port = int(os.environ.get("PORT", 8000))
-# Usa 'port' en lugar de 8000 al iniciar tu servidor
 
 @app.route('/')
-def index():
-    return "Hello, world!"
-
-@app.route('/test')
-def test():
-    return "This is a test endpoint"
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
+
