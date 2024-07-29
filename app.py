df2db10e4c134d435f79bcf7c7c662a5ffa7032a
Christian Lopez <cflopez.86@gmail.com>
2024-07-28 21:21:26 -0300

    Remove secret key for testing

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template, request
-import os
+from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY", "default_secret_key")
 
 @app.route('/')
 def index():
     return "Hello, world!"
 
+@app.route('/test')
+def test():
+    return "This is a test endpoint"
+
 if __name__ == '__main__':
-    app.run()
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8000)
